8d252a133a64e92f56379154801d7ead7aa93e5f
arjunn881 <arjunn881@gmail.com>
2024-11-05 01:08:21 +0530

    updated

--- nestedloop/prob2.c
+++ nestedloop/prob2.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-    for (int i = 6; i >= 1; i--) {         
-        for (int j = i; j >= 1; j--) { 
+    for (int i = 6; i >= 1; i--)
+    {
+        for (int j = i; j >= 1; j--)
+        {
 
-
-                printf("%d", j);
- 
+            printf("%d", j);
         }
         printf("\n");
     }
